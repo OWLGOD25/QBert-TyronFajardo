42e6e3719161d45ae38c357b5c1d95870f37eada
Tyron Fajardo <tyronjf44@gmail.com>
2026-01-21 10:46:04 -0500

    Adding the project to the repo

---
+++ Assets/My Assets/Scripts/ElevatorStopScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorStopScript : MonoBehaviour {
+	public string elevatorTag;
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == elevatorTag) {
+			other.GetComponent<Rigidbody2D> ().linearVelocity = Vector2.zero;
+			Destroy(other.gameObject);
+		}
+	}
+
+}
